47a0347742e7ebb11bcba45b41d03efe129368b2
Kriti-the-coder <89910822+Kriti-the-coder@users.noreply.github.com>
2022-02-09 15:23:55 +0530

    Add files via upload

--- game_login.js
+++ game_login.js
@@ -1,8 +1,8 @@
-
-function login(){
-    var name_of_player1= document.getElementById("player1_name_input").value;
-    var name_of_player2= document.getElementById("player2_name_input").value;
-    localStorage.setItem("player1_", name_of_player1 );
-    localStorage.setItem("name2",name_of_player2 );
-    window.location= ("game_page.html");
+function addUser()
+{
+    player1_name = document.getElementById("player1_name_input").value;
+    player2_name = document.getElementById("player2_name_input").value;
+    localStorage.setItem("player1_name", player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location.replace("game_page.html");
 }
